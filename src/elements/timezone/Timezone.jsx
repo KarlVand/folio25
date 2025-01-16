2feb0866d319a61a678e4769e9397ae37f022a93
KarlVand <cg.vandooren.karl@gmail.com>
2025-01-16 23:17:25 +0100

    Timezone - css cleaned up / hour display sorted

--- src/elements/timezone/Timezone.jsx
+++ src/elements/timezone/Timezone.jsx
@@ -7,10 +7,32 @@ import './timezone.css';
 
 function Timezone(){
 
+    /* Hour, minutes and seconds */
+    const time = () => {
+        let span = document.getElementById('span');
+        
+        let date = new Date();
+        let hours = date.getHours();
+        let minutes = date.getMinutes();
+        let seconds = date.getSeconds();
+        minutes = checkTime(minutes);
+        seconds = checkTime(seconds);
+        span.textContent = `${hours}:${minutes}:${seconds}`;
+    } 
+
+    setInterval(time, 1000);
+
+    /* Add zero in front of numbers < 10 */
+    const checkTime = (i) => {
+        if (i < 10) {i = "0" + i};  
+    return i;
+    }
+
     return (
         <div id='timeContainer'>
-            <h1>CET</h1>
-            <h1>, Brussels</h1>
+            <span id='span'></span>
+            <span className='display cet'>CET</span>
+            <span className='display brussels'>, Brussels</span>
         </div>
     )
 }
